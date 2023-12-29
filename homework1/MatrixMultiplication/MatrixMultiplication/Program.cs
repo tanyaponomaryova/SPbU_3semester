f76857de73bb8222661675af2215a7fc73ce3983
tanyaponomareva <tat.ponom2004@gmail.com>
2023-12-29 15:44:41 +0300

    corrections after review

--- homework1/MatrixMultiplication/MatrixMultiplication/Program.cs
+++ homework1/MatrixMultiplication/MatrixMultiplication/Program.cs
@@ -2,11 +2,11 @@
 
 Console.WriteLine("This program performs multiplication of " +
     "two matrices given in files \"firstMatrix.txt\" and \"secondMatrix.txt\".");
-// пути и exs
+
 try
 {
-    var firstMatrix = Matrix.GetMatrixFromFile("../../../firstMatrix.txt");
-    var secondMatrix = Matrix.GetMatrixFromFile("../../../secondMatrix.txt");
+    var firstMatrix = Matrix.GetMatrixFromFile("firstMatrix.txt");
+    var secondMatrix = Matrix.GetMatrixFromFile("secondMatrix.txt");
     var resultMatrixSeq = Matrix.SequentialMatrixMultiplication(firstMatrix, secondMatrix);
     var resultMatrixPar = Matrix.ParallelMatrixMultiplication(firstMatrix, secondMatrix);
 
@@ -21,7 +21,7 @@ try
     Console.WriteLine("To run a comparison of sequential and parallel multiplication, " +
         "enter the number of runs for each test case, to exit press any other key.");
 }
-catch (Exception e)
+catch (ArgumentException e)
 {
     Console.WriteLine("An error occurred during program execution:");
     Console.WriteLine(e.Message);
@@ -30,14 +30,7 @@ catch (Exception e)
 
 if (int.TryParse(Console.ReadLine(), out int numberOfTestRuns))
 {
-    try
-    {
-        Console.WriteLine("Comparison experiment is running...");
-        ComparisonPerformanceOfTwoMultiplicationAlgorithms.RunExperiment(numberOfTestRuns, "../../../experimentResults.txt");
-        Console.WriteLine("Results of experiment was written to file \"experimentResults.txt\".");
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e.Message);
-    }
+    Console.WriteLine("Comparison experiment is running...");
+    ComparisonPerformanceOfTwoMultiplicationAlgorithms.RunExperiment(numberOfTestRuns, "../../../experimentResults.txt");
+    Console.WriteLine("Results of experiment was written to file \"experimentResults.txt\".");
 }
